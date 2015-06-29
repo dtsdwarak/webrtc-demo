cd7f058b6d37e5188693de78696a790e9f43c4e9
Dwaraka Nath <dtsdwarak@hotmail.com>
2015-06-29 13:41:57 +0530

    reverting back

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ var app = express();
 
 app.use(express.static(__dirname));
 
-var port = process.env.PORT || 8000;
-//var port1 = process.env.PORT || 8001;
-https.createServer({key: privateKey, cert: certificate}, app).listen(port);
-//http.createServer(app).listen(port1);
+//var port = process.env.PORT || 8000;
+var port1 = process.env.PORT || 8001;
+//https.createServer({key: privateKey, cert: certificate}, app).listen(port);
+http.createServer(app).listen(port1);
 
 console.log('running on https://localhost:8000 and http://localhost:8001');
